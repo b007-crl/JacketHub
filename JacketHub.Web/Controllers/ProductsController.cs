99a5112f4522ed6cfbd813148483d8fca2a97bb3
bilalkhan.bk23 <bilalkhan.bk23@outlook.com>
2020-06-30 07:57:06 -0700

    created product service for crud operations and controller for product in process

---
+++ JacketHub.Web/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using jacketHub.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JacketHub.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        ProductServices services = new ProductServices();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductTable()
+        {
+           var products =  services.GetProducts();
+            return View(products);
+        }
+    }
+}
---
+++ jacketHub.Services/ProductServices.cs
@@ -0,0 +1,62 @@
+﻿using JacketHub.DataBase;
+using JacketHub.Entitties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace jacketHub.Services
+{
+    public class ProductServices
+    {
+        public List<Product> GetProducts()
+        {
+            using(var context = new JHContext())
+            {
+                return context.Products.ToList();
+            }
+
+        }
+
+        public Product GetProduct(int ID)
+        {
+            using (var context = new JHContext())
+            {
+                return context.Products.Find(ID);
+            }
+
+        }
+
+        public void SaveCategory(Product product)
+        {
+            using (var context = new JHContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new JHContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new JHContext())
+            {
+                var product = context.Products.Find(ID);
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+
+        }
+    } 
+}
